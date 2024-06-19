11b87881b91000f338274524c234a11808fc9b24
Shashank Chauhan <shashank.chauhan@comviva.com>
2024-06-19 17:10:11 +0530

    Parameterized Constructors

---
+++ OOPs/src/main/java/org/example/constructors/Person.java
@@ -0,0 +1,12 @@
+package org.example.constructors;
+
+public class Person {
+    private int age;
+    private String name;
+
+    public Person(int age,String name) {
+        this.age=age;
+        this.name=name;
+    }
+}
+
